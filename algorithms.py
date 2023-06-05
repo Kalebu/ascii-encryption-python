52a96f11b4adc019b7782d59fc16a5a3fbb3056c
1Codealot <58225804+1Codealot@users.noreply.github.com>
2023-06-05 19:40:42 +0100

    Update algorithms.py
    Basically there were numbers that were pulled fresh from my butt which made it annoying to add the encrypted text to a new file.

--- algorithms.py
+++ algorithms.py
@@ -7,14 +7,14 @@ class __crypt:
         seed(key)
         if not isinstance(text, str):
             raise TypeError("{} should be a type string".format(text))
-        return "".join([chr(ord(something) + randint(1,randint(2,30))) for something in text])
+        return "".join([chr(ord(something) + randint(1,randint(2,10))) for something in text])
     
     def decrypt(text, key):
         """Decrypts the text, key is used as seed and must be given and must be the same as the encryption."""
         seed(key)
         if not isinstance(text, str):
             raise TypeError("{} should be a type string".format(text))
-        return "".join([chr(ord(something) - randint(1,randint(2,30))) for something in text])
+        return "".join([chr(ord(something) - randint(1,randint(2,10))) for something in text])
     
 if __name__ == '__main__':
     a = __crypt.encrypt("Hello world", 5)
